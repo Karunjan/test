8b22034762c4eadae0444fba8d601e310efb27c3
Karunjan <rkarunjan@icloud.com>
2024-12-06 17:56:28 +0530

    Initial commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,23 +6,49 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { CommonModule } from '@angular/common';
 // import { AboutComponent } from "../about/about.component";
 import { FormPageComponent } from "../contact/contact.component";
-// import { ServiceComponent } from "../service/service.component";
-
+import { ServiceComponent } from "../service/service.component";
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 
 
 @Component({
   selector: 'app-header',
   standalone:true,
-  imports: [NzLayoutModule, NzIconModule, CommonModule, NzMenuModule, FormPageComponent],
+  imports: [NzLayoutModule,NzInputModule, NzIconModule,NzGridModule, CommonModule,FormsModule, NzAvatarModule, NzMenuModule, FormPageComponent,ServiceComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  showEditForm = false;
+  showForms = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  
+  closeEditForm() {
+    this.showForms = false; // Toggle the form visibility
   }
+
+  toggleForm(): void {
+    this.showEditForm = !this.showEditForm;
+  }
+
+  toggleForms(): void {
+    this.showForms = !this.showForms;
+  }
+
+  handleSubmit(): void {
+    this.showEditForm = false; 
+  }
+
+  handleSubmits(): void {
+    this.showForms = false; 
+  }
+
+  // handleSubmits() {
+  //   console.log('Form submitted');
+  //   this.toggleForm(); 
+  // }
 
 }
