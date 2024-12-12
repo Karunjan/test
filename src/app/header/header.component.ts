899230c7ecaa9b5489d460f4c8ea7e235ea70e70
Karunjan <karunjanr@outlook.com>
2024-12-12 14:27:37 +0530

    changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,27 +5,58 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 // import { HomeComponent } from '../home/home.component';
 import { CommonModule } from '@angular/common';
 // import { AboutComponent } from "../about/about.component";
-import { FormPageComponent } from "../contact/contact.component";
-import { ServiceComponent } from "../service/service.component";
+import { FormPageComponent } from '../contact/contact.component';
+import { ServiceComponent } from '../service/service.component';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzInputModule } from 'ng-zorro-antd/input';
-
-
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-header',
-  standalone:true,
-  imports: [NzLayoutModule,NzInputModule, NzIconModule,NzGridModule, CommonModule,FormsModule, NzAvatarModule, NzMenuModule, FormPageComponent,ServiceComponent],
+  standalone: true,
+  imports: [
+    NzLayoutModule,
+    NzInputModule,
+    NzAlertModule,
+    NzButtonModule,
+    NzIconModule,
+    NzGridModule,
+    CommonModule,
+    FormsModule,
+    NzAvatarModule,
+    NzMenuModule,
+    FormPageComponent,
+    ServiceComponent,
+  ],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   showEditForm = false;
   showForms = false;
+  showSuccessMessage = false; // State to track if success message should be shown
+  isFormOpen = true; // State to track if form is open
 
-  
+  createMessage(type: string): void {
+    this.message.create(type, `Your Profile  ${type}`);
+  }
+
+  constructor(private message: NzMessageService) {}
+
+  handleSuccess(): void {
+    this.showForms = false; // Hide the form
+    this.showSuccessMessage = true; // Show the success message
+
+    // Auto-hide success message after 3 seconds (optional)
+    setTimeout(() => {
+      this.showSuccessMessage = false;
+    }, 3000); // 3 seconds
+  }
+
   closeEditForm() {
     this.showForms = false; // Toggle the form visibility
   }
@@ -39,16 +70,15 @@ export class HeaderComponent {
   }
 
   handleSubmit(): void {
-    this.showEditForm = false; 
+    this.showEditForm = false;
   }
 
   handleSubmits(): void {
-    this.showForms = false; 
+    this.showForms = false;
   }
 
   // handleSubmits() {
   //   console.log('Form submitted');
-  //   this.toggleForm(); 
+  //   this.toggleForm();
   // }
-
 }
--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-service',
-  standalone:true,
+  standalone: true,
+  imports: [
+    NzLayoutModule,
+    NzAvatarModule,
+    NzButtonModule,
+    NzSwitchModule,
+    NzIconModule,
+    NzMenuModule,
+    CommonModule,
+    NzToolTipModule,
+    FormsModule,
+  ],
+
   templateUrl: './service.component.html',
-  styleUrls: ['./service.component.css']
+  styleUrls: ['./service.component.css'],
 })
-export class ServiceComponent implements OnInit {
+export class ServiceComponent {
+  theme = true;
+  isCollapsed = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  toggleSidebar(): void {
+    this.isCollapsed = !this.isCollapsed;
   }
-
 }
